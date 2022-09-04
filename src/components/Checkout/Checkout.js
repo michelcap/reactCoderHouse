81957d2c5f3b0d6e56fd4dfbd5386ad6b6b983aa
michelCoderHouse <m.s.capurro.delbono@gmail.com>
2022-09-04 14:06:02 -0500

    casiFinal

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import { addDoc, collection, getDocs, query, where, writeBatch } from 'firebase/firestore';
+import { addDoc, collection, getDocs, query, where, writeBatch, documentId } from 'firebase/firestore';
 import { useState, useContext } from 'react';
 import { db } from '../../services/firebase';
 import CartContext from '../../context/CartContext';
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 const Checkout = () => {
 
     const [isLoading, setIsLoading] = useState(false);
-    const [isCheck, setIsCheck] = useState(false);
+    const [out, setOut] = useState();
     const [outOf, setOutOf] = useState(false);
     const [emptyForm, setEmptyForm] = useState(false);
     const [orderCreated, setOrderCreated] = useState(false);
@@ -41,47 +41,14 @@ const Checkout = () => {
         }
     };
 
-    // const awaitCheck = async (refAll, prod, outOfStock) => {
-
-    //     await getDocs(refAll)
-    //         .then(response => {
-    //             const prodMach = response.docs.map(doc => {
-    //                 const batch = writeBatch(db);
-    //                 // batch.update(doc.ref, {'order_backend': 50})
-    //                 // batch.commit();
-    //                 const data = doc.data();
-    //                 const inStock = data.order_backend;
-    //                 const quantityProductInCart = getProductQuantity(prod.id);
-    //                 if (inStock >= quantityProductInCart) {
-    //                     const descount = inStock - quantityProductInCart;
-    //                     batch.update(doc.ref, { 'order_backend': descount });
-    //                 } else {
-    //                     outOfStock.push({ id: prod.id, title: prod.title });
-    //                 }
-    //                 if (outOfStock.length === 0) {
-    //                     batch.commit();
-    //                 }
-    //                 return (outOfStock.length);
-    //             });
-    //             return (prodMach);
-    //         })
-    //         .catch(error => { console.log(error); });
-
-    //     return(true)
-
-
-    // };
-
     const createOrder = async () => {
-        const outOfStock = [];
-        console.log(outOfStock);
         setIsLoading(true);
         setEmptyForm(false);
         const buyer = getBuyer();
         if (isObjEmpty(buyer)) {
             setTimeout(() => {
                 navigate('/cart');
-            }, 7000);
+            }, 6000);
             setIsLoading(false);
             setEmptyForm(true);
         } else {
@@ -94,78 +61,69 @@ const Checkout = () => {
                     date: new Date()
                 };
 
-                const machProduct = cart.map(prod => {
-                    const refAll = query(collection(db, 'products'), where('id', '==', prod.id));
-                    // awaitCheck(refAll, prod, outOfStock);
- 
-                    getDocs(refAll)
-                        .then(response => {
-                            isCheck = false
-                            const prodMach = response.docs.map(doc => {
-                                const batch = writeBatch(db);
-                                // batch.update(doc.ref, {'order_backend': 50})
-                                // batch.commit();
-                                const data = doc.data();
-                                const inStock = data.order_backend;
-                                const quantityProductInCart = getProductQuantity(prod.id);
+                const idCart = cart.map(prod => prod.id);
+
+                const colletionRef = collection(db, 'products');
+
+                const inFirestone = [];
+                idCart.forEach(id => {
+                    const ref = query(colletionRef, where('id', '==', id));
+                    const productFromFirestone = getDocs(ref)
+                        .then(res => res.docs.map(doc => {
+                            const data = doc.data();
+                            const stock = data.order_backend;
+                            const title = data.title;
+                            const docRef = doc.ref;
+                            return ({ id: id, title: title, stock: stock, docRef: docRef });
+                        }));
+                    inFirestone.push(productFromFirestone);
+                });
+
+                const batch = writeBatch(db);
+                const outOfStock = [];
+                Promise.all(inFirestone).then(value => {
+                    value.map(fireProd => {
+                        cart.map(cartProd => {
+                            if (fireProd[0].id === cartProd.id) {
+                                const inStock = fireProd[0].stock;
+                                const quantityProductInCart = getProductQuantity(cartProd.id);
                                 if (inStock >= quantityProductInCart) {
                                     const descount = inStock - quantityProductInCart;
-                                    batch.update(doc.ref, { 'order_backend': descount });
+                                    const ref = fireProd[0].docRef;
+                                    batch.update(ref, { 'order_backend': descount });
                                 } else {
-                                    outOfStock.push({ id: prod.id, title: prod.title });
+                                    const id = cartProd.id;
+                                    const title = cartProd.title;
+                                    outOfStock.push({ id: id, title: title });
                                 }
-                                if (outOfStock.length === 0) {
-                                    batch.commit();
-                                }
-                                return (outOfStock);
-                            });
-                            isCheck = true
-                            return (prodMach);
-                        }).catch(error => { console.log(error); });
-
-
-                    // if (outOfStock.length === 0) {
-                    //     const refOrder = collection(db, 'orders');
-                    //     awaitId(refOrder, objOrder);
-                    //     setTimeout(() => {
-                    //         navigate('/');
-                    //     }, 6000);
-                    //     clearCart();
-                    //     setOrderCreated(true);
-                    // } else {
-                    //     setTimeout(() => {
-                    //         navigate('/cart');
-                    //     }, 5000);
-                    //     setOutOf(true);
-                    //     setIsLoading(false);
-                    // }
-
-                    
-                    console.log(isCheck)
-                    if (true) {
-                        if (outOfStock.length === 0) {
-                            const refOrder = collection(db, 'orders');
-                            awaitId(refOrder, objOrder);
-                            setTimeout(() => {
-                                navigate('/');
-                            }, 6000);
-                            clearCart();
-                            setOrderCreated(true);
-                        } else {
-                            setTimeout(() => {
-                                navigate('/cart');
-                            }, 5000);
-                            setOutOf(true);
-                            setIsLoading(false);
-                        }
+                            }
+                        });
+                    });
+                    if (outOfStock.length === 0) {
+                        batch.commit();
+                        const refOrder = collection(db, 'orders');
+                        awaitId(refOrder, objOrder);
+                        setTimeout(() => {
+                            navigate('/');
+                        }, 6000);
+                        clearCart();
+                        setOrderCreated(true);
+                    } else {
+                        setOut(outOfStock);
+                        setTimeout(() => {
+                            navigate('/cart');
+                        }, 7000);
+                        setOutOf(true);
+                        setIsLoading(false);
                     }
-                    return (outOfStock.length);
                 });
-                return (machProduct);
+
             } catch (error) {
                 console.log(error);
             } finally {
+
             }
+
         }
     };
 
@@ -185,7 +143,12 @@ const Checkout = () => {
                         <h4 class='h6 text-white'>ERROR !</h4>
                     </heder>
                     <section class="card-body">
-                        <p class="card-text fs-6">Hay productos fuera de stock, sera redirigido al carrito en 5 segundos</p>
+                        <p class="card-text fs-6">Hay productos fuera de stock, sera redirigido al carrito en 6 segundos</p>
+                    </section>
+                    <section class="card-body">
+                        {out.map(prod =>
+                            <p class="card-text fs-6">{prod.title}</p>
+                        )}
                     </section>
                 </article>
             </div>
@@ -200,7 +163,7 @@ const Checkout = () => {
                         <h4 class='h6 text-white'>Alerta !</h4>
                     </heder>
                     <section class="card-body">
-                        <p class="card-text fs-6">Hay datos faltantes o incorrectos en el Formulario de datos Personales, sera redirigido al carrito en 7 segundos</p>
+                        <p class="card-text fs-6">Hay datos faltantes o incorrectos en el Formulario de datos Personales, sera redirigido al carrito en 5 segundos</p>
                     </section>
                 </article>
             </div>
--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -4,18 +4,37 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
+  // ORIGINAL
   // apiKey: "AIzaSyAtTucFTIIfR7JZg8i-5MOcf12mSNy0va0",
   // authDomain: "backendmichelcapurro.firebaseapp.com",
   // projectId: "backendmichelcapurro",
   // storageBucket: "backendmichelcapurro.appspot.com",
   // messagingSenderId: "825295285682",
   // appId: "1:825295285682:web:d90d40392484b76396c8ab"
-  apiKey: "AIzaSyCZrQ4QHVrWq2DByecW1yBq3RrGPybpMRQ",
-  authDomain: "prueba-80b87.firebaseapp.com",
-  projectId: "prueba-80b87",
-  storageBucket: "prueba-80b87.appspot.com",
-  messagingSenderId: "352116822197",
-  appId: "1:352116822197:web:deed67b3ac2d963061eefc"
+
+  // SECUNDARIA 1
+  // apiKey: "AIzaSyCZrQ4QHVrWq2DByecW1yBq3RrGPybpMRQ",
+  // authDomain: "prueba-80b87.firebaseapp.com",
+  // projectId: "prueba-80b87",
+  // storageBucket: "prueba-80b87.appspot.com",
+  // messagingSenderId: "352116822197",
+  // appId: "1:352116822197:web:deed67b3ac2d963061eefc"
+
+  // SECUNDARIA 2
+  // apiKey: "AIzaSyAkcMRWJSyVxd8f2K8j5_m7Em5The0_ePc",
+  // authDomain: "nnnn-5bcb0.firebaseapp.com",
+  // projectId: "nnnn-5bcb0",
+  // storageBucket: "nnnn-5bcb0.appspot.com",
+  // messagingSenderId: "657650829229",
+  // appId: "1:657650829229:web:f64f4f034eb116d5c1c324"
+
+  // SUCUNDARIA 3
+  apiKey: "AIzaSyBYxMSQFby-S2-7Tx6Ik6h7KCbjfiOsw3c",
+  authDomain: "fir-a1bce.firebaseapp.com",
+  projectId: "fir-a1bce",
+  storageBucket: "fir-a1bce.appspot.com",
+  messagingSenderId: "1014695952009",
+  appId: "1:1014695952009:web:f5a9321c9a841748e51058"
 };
 
 // Initialize Firebase
